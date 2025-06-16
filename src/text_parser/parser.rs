614924a239d1437a5a0c4ddd4d05876ab97128d9
redlintles <treelands54@gmail.com>
2025-06-16 20:26:06 -0300

    Processo de atualização individual dos tokens para usar AtpError: text_parser/parser Atualizado

--- src/text_parser/parser.rs
+++ src/text_parser/parser.rs
@@ -1,5 +1,5 @@
-use crate::token_data::{ TokenMethods };
+use crate::{ token_data::TokenMethods, utils::errors::AtpError };
 
-pub fn parse_token(token: &dyn TokenMethods, input: &str) -> String {
+pub fn parse_token(token: &dyn TokenMethods, input: &str) -> Result<String, AtpError> {
     token.parse(input)
 }
