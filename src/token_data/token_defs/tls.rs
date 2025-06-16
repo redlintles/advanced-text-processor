43bfd91f00b5b373f2564d65ba5e4fa39619b7ba
redlintles <treelands54@gmail.com>
2025-06-16 20:24:15 -0300

    Processo de atualização individual dos tokens para usar AtpError: Tls Atualizado

--- src/token_data/token_defs/tls.rs
+++ src/token_data/token_defs/tls.rs
@@ -1,7 +1,10 @@
 use crate::token_data::TokenMethods;
 
 #[cfg(feature = "bytecode")]
-use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::{
+    bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods },
+    utils::errors::AtpError,
+};
 // Trim left side
 #[derive(Clone, Copy, Default)]
 pub struct Tls {}
@@ -11,16 +14,24 @@ impl TokenMethods for Tls {
         "tls;\n".to_string()
     }
 
-    fn parse(&self, input: &str) -> String {
-        String::from(input.trim_start())
+    fn parse(&self, input: &str) -> Result<String, AtpError> {
+        Ok(String::from(input.trim_start()))
     }
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String> {
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), AtpError> {
         // "tls;"
 
         if line[0] == "tls" {
             return Ok(());
         }
-        Err("Parsing Error".to_string())
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::TokenNotFound(
+                    "Invalid parser for this token".to_string()
+                ),
+                line[0].to_string(),
+                line.join(" ")
+            )
+        )
     }
     fn get_string_repr(&self) -> String {
         "tls".to_string()
@@ -31,12 +42,20 @@ impl BytecodeTokenMethods for Tls {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: BytecodeInstruction
-    ) -> Result<(), String> {
+    ) -> Result<(), AtpError> {
+        use crate::utils::errors::AtpErrorCode;
+
         if instruction.op_code == Tls::default().get_opcode() {
             return Ok(());
         }
 
-        Err("An ATP Bytecode parsing error ocurred: Invalid Token".to_string())
+        Err(
+            AtpError::new(
+                AtpErrorCode::BytecodeNotFound("".to_string()),
+                instruction.op_code.to_string(),
+                instruction.operands.join(" ")
+            )
+        )
     }
 
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
