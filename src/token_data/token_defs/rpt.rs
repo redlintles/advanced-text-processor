58c583a02f9ff17100a77275516d5e2d6c40bd72
redlintles <treelands54@gmail.com>
2025-06-16 20:11:07 -0300

    Processo de atualização individual dos tokens para usar AtpError: Rpt Atualizado

--- src/token_data/token_defs/rpt.rs
+++ src/token_data/token_defs/rpt.rs
@@ -1,4 +1,7 @@
-use crate::{ token_data::TokenMethods, utils::transforms::string_to_usize };
+use crate::{
+    token_data::TokenMethods,
+    utils::{ errors::{ AtpError, AtpErrorCode }, transforms::string_to_usize },
+};
 
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
@@ -18,16 +21,24 @@ impl TokenMethods for Rpt {
         format!("rpt {};\n", self.times)
     }
 
-    fn parse(&self, input: &str) -> String {
-        input.repeat(self.times)
+    fn parse(&self, input: &str) -> Result<String, AtpError> {
+        Ok(input.repeat(self.times))
     }
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String> {
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), AtpError> {
         if line[0] == "rpt" {
             self.times = string_to_usize(&line[1])?;
             return Ok(());
         }
 
-        Err("Parsing Error".to_string())
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::TokenNotFound(
+                    "Invalid parser for this token".to_string()
+                ),
+                line[0].to_string(),
+                line.join(" ")
+            )
+        )
     }
 
     fn get_string_repr(&self) -> String {
@@ -39,17 +50,31 @@ impl BytecodeTokenMethods for Rpt {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: BytecodeInstruction
-    ) -> Result<(), String> {
+    ) -> Result<(), AtpError> {
         if instruction.op_code == Rpt::default().get_opcode() {
             if !(instruction.operands[0].is_empty() || instruction.operands[1].is_empty()) {
                 self.times = string_to_usize(&instruction.operands[1])?;
                 return Ok(());
             }
 
-            return Err("An ATP Bytecode parsing error ocurred: Invalid Operands".to_string());
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::InvalidOperands(
+                        "Invalid operands for this instruction".to_string()
+                    ),
+                    instruction.op_code.to_string(),
+                    instruction.operands.join(" ")
+                )
+            );
         }
 
-        Err("An ATP Bytecode parsing error ocurred: Invalid Token".to_string())
+        Err(
+            AtpError::new(
+                AtpErrorCode::BytecodeNotFound("".to_string()),
+                instruction.op_code.to_string(),
+                instruction.operands.join(" ")
+            )
+        )
     }
 
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
