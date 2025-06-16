f583e7a78177104e27cb527b810f735e4593e865
redlintles <treelands54@gmail.com>
2025-06-16 19:42:13 -0300

    Processo de atualização individual dos tokens para usar AtpError: Dlc Atualizado

--- src/token_data/token_defs/dlc.rs
+++ src/token_data/token_defs/dlc.rs
@@ -1,7 +1,8 @@
-use crate::{ token_data::TokenMethods, utils::transforms::string_to_usize };
+use crate::{ token_data::TokenMethods, utils::{ errors::AtpError, transforms::string_to_usize } };
 
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::utils::errors::AtpErrorCode;
 // Delete Chunk
 #[derive(Clone, Copy, Default)]
 pub struct Dlc {
@@ -23,7 +24,7 @@ impl TokenMethods for Dlc {
         format!("dlc {} {};\n", self.start_index, self.end_index)
     }
 
-    fn parse(&self, input: &str) -> String {
+    fn parse(&self, input: &str) -> Result<String, AtpError> {
         if self.start_index <= self.end_index {
             input.to_string();
         }
@@ -31,19 +32,43 @@ impl TokenMethods for Dlc {
             .char_indices()
             .nth(self.start_index)
             .map(|(i, _)| i)
-            .unwrap_or(input.len());
+            .ok_or_else(||
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::IndexOutOfRange(
+                        format!(
+                            "Invalid Index for this specific input, supported indexes 0-{}, entered index {}",
+                            input.len() - 1,
+                            self.start_index
+                        )
+                    ),
+                    self.token_to_atp_line(),
+                    input.to_string()
+                )
+            )?;
         let end_index = input
             .char_indices()
             .nth(self.end_index)
             .map(|(i, _)| i)
-            .unwrap_or(input.len());
+            .ok_or_else(||
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::IndexOutOfRange(
+                        format!(
+                            "Invalid Index for this specific input, supported indexes 0-{}, entered index {}",
+                            input.len() - 1,
+                            self.end_index
+                        )
+                    ),
+                    self.token_to_atp_line(),
+                    input.to_string()
+                )
+            )?;
 
         let before = &input[..start_index.min(input.len())];
         let after = &input[end_index.min(input.len())..];
 
-        format!("{}{}", before, after)
+        Ok(format!("{}{}", before, after))
     }
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String> {
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), AtpError> {
         // "dlc;"
 
         if line[0] == "dlc" {
@@ -51,7 +76,15 @@ impl TokenMethods for Dlc {
             self.end_index = string_to_usize(&line[2])?;
             return Ok(());
         }
-        Err("Parsing Error".to_string())
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::TokenNotFound(
+                    "Invalid parser for this token".to_string()
+                ),
+                line[0].to_string(),
+                line.join(" ")
+            )
+        )
     }
 
     fn get_string_repr(&self) -> String {
@@ -63,7 +96,7 @@ impl BytecodeTokenMethods for Dlc {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: BytecodeInstruction
-    ) -> Result<(), String> {
+    ) -> Result<(), AtpError> {
         if instruction.op_code == Dlc::default().get_opcode() {
             if !instruction.operands[0].is_empty() {
                 self.start_index = string_to_usize(&instruction.operands[1])?;
@@ -71,10 +104,24 @@ impl BytecodeTokenMethods for Dlc {
                 return Ok(());
             }
 
-            return Err("An ATP Bytecode parsing error ocurred: Invalid Operands".to_string());
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::InvalidOperands(
+                        "Invalid operands for this instruction".to_string()
+                    ),
+                    instruction.op_code.to_string(),
+                    instruction.operands.join(" ")
+                )
+            );
         }
 
-        Err("An ATP Bytecode parsing error ocurred: Invalid Token".to_string())
+        Err(
+            AtpError::new(
+                AtpErrorCode::BytecodeNotFound("".to_string()),
+                instruction.op_code.to_string(),
+                instruction.operands.join(" ")
+            )
+        )
     }
 
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
