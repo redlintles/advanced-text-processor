d38616c402676c21937d461698a1c40177b8cc3a
redlintles <treelands54@gmail.com>
2025-06-16 20:13:48 -0300

    Processo de atualização individual dos tokens para usar AtpError: Rtl Atualizado

--- src/token_data/token_defs/rtl.rs
+++ src/token_data/token_defs/rtl.rs
@@ -1,7 +1,10 @@
 use crate::{ token_data::TokenMethods, utils::transforms::string_to_usize };
 
 #[cfg(feature = "bytecode")]
-use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::{
+    bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods },
+    utils::errors::{ AtpError, AtpErrorCode },
+};
 #[derive(Clone, Default)]
 pub struct Rtl {
     pub times: usize,
@@ -16,30 +19,48 @@ impl Rtl {
 }
 
 impl TokenMethods for Rtl {
-    fn parse(&self, input: &str) -> String {
+    fn parse(&self, input: &str) -> Result<String, AtpError> {
         if input.is_empty() {
-            return String::default();
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::InvalidParameters(
+                        "Input is empty".to_string()
+                    ),
+                    self.token_to_atp_line(),
+                    "\" \"".to_string()
+                )
+            );
         }
 
         let chars: Vec<char> = input.chars().collect();
         let len = chars.len();
         let times = self.times % len;
 
-        chars[times..]
-            .iter()
-            .chain(&chars[..times])
-            .collect()
+        Ok(
+            chars[times..]
+                .iter()
+                .chain(&chars[..times])
+                .collect()
+        )
     }
 
     fn token_to_atp_line(&self) -> String {
         format!("rtl {};\n", self.times)
     }
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String> {
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), AtpError> {
         if line[0] == "rtl" {
             self.times = string_to_usize(&line[1])?;
             return Ok(());
         }
-        Err("Parsing Error".to_string())
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::TokenNotFound(
+                    "Invalid parser for this token".to_string()
+                ),
+                line[0].to_string(),
+                line.join(" ")
+            )
+        )
     }
 
     fn get_string_repr(&self) -> String {
@@ -51,17 +72,31 @@ impl BytecodeTokenMethods for Rtl {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: BytecodeInstruction
-    ) -> Result<(), String> {
+    ) -> Result<(), AtpError> {
         if instruction.op_code == Rtl::default().get_opcode() {
             if !(instruction.operands[0].is_empty() || instruction.operands[1].is_empty()) {
                 self.times = string_to_usize(&instruction.operands[1])?;
                 return Ok(());
             }
 
-            return Err("An ATP Bytecode parsing error ocurred: Invalid Operands".to_string());
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::InvalidOperands(
+                        "Invalid operands for this instruction".to_string()
+                    ),
+                    instruction.op_code.to_string(),
+                    instruction.operands.join(" ")
+                )
+            );
         }
 
-        Err("An ATP Bytecode parsing error ocurred: Invalid Token".to_string())
+        Err(
+            AtpError::new(
+                AtpErrorCode::BytecodeNotFound("".to_string()),
+                instruction.op_code.to_string(),
+                instruction.operands.join(" ")
+            )
+        )
     }
 
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
