28618aa1fbda770b71b36a6102442718b7aa3670
redlintles <treelands54@gmail.com>
2025-06-16 20:48:21 -0300

    Processo de atualização individual dos tokens para usar AtpError: bytecode_parser/reader Atualizado

--- src/bytecode_parser/reader.rs
+++ src/bytecode_parser/reader.rs
@@ -1,6 +1,7 @@
 use std::{ fs::OpenOptions, io::{ BufRead, BufReader }, path::Path };
 
 use crate::utils::{
+    errors::{ AtpError, AtpErrorCode },
     mapping::get_mapping_bytecode_to_token,
     transforms::token_from_hex_string,
     validations::check_file_path,
@@ -8,14 +9,24 @@ use crate::utils::{
 
 use super::{ BytecodeInstruction, BytecodeTokenMethods };
 
-pub fn read_bytecode_from_file(path: &Path) -> Result<Vec<Box<dyn BytecodeTokenMethods>>, String> {
+pub fn read_bytecode_from_file(
+    path: &Path
+) -> Result<Vec<Box<dyn BytecodeTokenMethods>>, AtpError> {
     check_file_path(path, Some("atpbc"))?;
     let mut result: Vec<Box<dyn BytecodeTokenMethods>> = Vec::new();
 
     let file = match OpenOptions::new().read(true).open(path) {
         Ok(x) => x,
         Err(_) => {
-            return Err("An Atp Bytecode reading error ocurred: Could not open file".to_string());
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::FileOpeningError(
+                        "Failed opening File".to_string()
+                    ),
+                    "".to_string(),
+                    format!("{:?}", path)
+                )
+            );
         }
     };
 
@@ -25,28 +36,60 @@ pub fn read_bytecode_from_file(path: &Path) -> Result<Vec<Box<dyn BytecodeTokenM
         let line_text = match line {
             Ok(x) => x,
             Err(_) => {
-                return Err("An ATP Parsing error ocurred: Error reading file line".to_string());
+                return Err(
+                    AtpError::new(
+                        crate::utils::errors::AtpErrorCode::FileReadingError(
+                            "Failed reading file line".to_string()
+                        ),
+                        "".to_string(),
+                        "".to_string()
+                    )
+                );
             }
         };
 
         let chunks = match shell_words::split(&line_text) {
             Ok(x) => x,
             Err(_) => {
-                return Err("An ATP Parsing error ocurred: Error splitting file line".to_string());
+                return Err(
+                    AtpError::new(
+                        AtpErrorCode::TextParsingError(
+                            "An ATP Parsing error ocurred: Error splitting file line".to_string()
+                        ),
+                        "shell words split".to_string(),
+                        line_text.to_string()
+                    )
+                );
             }
         };
 
         let brute_op_code = match chunks.first() {
             Some(x) => x,
             None => {
-                return Err("An ATP Parsing error ocurred: Parsing error".to_string());
+                return Err(
+                    AtpError::new(
+                        crate::utils::errors::AtpErrorCode::BytecodeParsingError(
+                            "Failed Parsing op_code".to_string()
+                        ),
+                        "".to_string(),
+                        chunks.join(" ")
+                    )
+                );
             }
         };
 
         let parsed_op_code = match token_from_hex_string(&brute_op_code) {
             Some(x) => x,
             None => {
-                return Err("An ATP Parsing error ocurred: Parsing error".to_string());
+                return Err(
+                    AtpError::new(
+                        crate::utils::errors::AtpErrorCode::BytecodeParsingError(
+                            "Failed Parsing op_code".to_string()
+                        ),
+                        "".to_string(),
+                        chunks.join(" ")
+                    )
+                );
             }
         };
 
@@ -60,7 +103,15 @@ pub fn read_bytecode_from_file(path: &Path) -> Result<Vec<Box<dyn BytecodeTokenM
         let mapped_token = match op_code_map.get(&(parsed_op_code as u8)) {
             Some(x) => x,
             None => {
-                return Err("An ATP Parsing error ocurred: Token not found".to_string());
+                return Err(
+                    AtpError::new(
+                        crate::utils::errors::AtpErrorCode::TokenNotFound(
+                            "Token not recognized".to_string()
+                        ),
+                        "".to_string(),
+                        parsed_op_code.to_string()
+                    )
+                );
             }
         };
 
@@ -69,7 +120,15 @@ pub fn read_bytecode_from_file(path: &Path) -> Result<Vec<Box<dyn BytecodeTokenM
         match new_token.token_from_bytecode_instruction(instruction) {
             Ok(()) => result.push(new_token),
             Err(_) => {
-                return Err("An ATP Parsing error ocurred: Token not found".to_string());
+                return Err(
+                    AtpError::new(
+                        crate::utils::errors::AtpErrorCode::TokenNotFound(
+                            "Token not recognized".to_string()
+                        ),
+                        "".to_string(),
+                        parsed_op_code.to_string()
+                    )
+                );
             }
         }
     }
