f3e295371d1afeb8b6c293a48b5b1b6c556c2c9b
redlintles <treelands54@gmail.com>
2025-06-16 20:21:29 -0300

    Processo de atualização individual dos tokens para usar AtpError: Slt Atualizado

--- src/token_data/token_defs/slt.rs
+++ src/token_data/token_defs/slt.rs
@@ -1,7 +1,10 @@
 use crate::{ token_data::TokenMethods, utils::transforms::string_to_usize };
 
 #[cfg(feature = "bytecode")]
-use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::{
+    bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods },
+    utils::errors::{ AtpError, AtpErrorCode },
+};
 #[derive(Clone, Default)]
 pub struct Slt {
     pub start_index: usize,
@@ -21,23 +24,47 @@ impl TokenMethods for Slt {
     fn get_string_repr(&self) -> String {
         "slt".to_string()
     }
-    fn parse(&self, input: &str) -> String {
+    fn parse(&self, input: &str) -> Result<String, AtpError> {
         if self.start_index <= self.end_index && self.end_index <= input.len() {
             if let Some(slice) = input.get(self.start_index..self.end_index) {
-                return slice.to_string();
-            } else {
-                input.to_string();
+                return Ok(slice.to_string());
             }
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::TextParsingError(
+                        "Failed slicing the desired input".to_string()
+                    ),
+                    self.token_to_atp_line(),
+                    input.to_string()
+                )
+            );
         }
-        input.to_string()
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::IndexOutOfRange(
+                    "Invalid bounds for the chunk to select".to_string()
+                ),
+                self.token_to_atp_line(),
+                input.to_string()
+            )
+        )
     }
 
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String> {
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), AtpError> {
         if line[0] == "slt" {
+            self.start_index = string_to_usize(&line[1])?;
+            self.end_index = string_to_usize(&line[2])?;
             return Ok(());
-        } else {
-            return Err("".to_string());
         }
+        Err(
+            AtpError::new(
+                crate::utils::errors::AtpErrorCode::TokenNotFound(
+                    "Invalid parser for this token".to_string()
+                ),
+                line[0].to_string(),
+                line.join(" ")
+            )
+        )
     }
 
     fn token_to_atp_line(&self) -> String {
@@ -54,14 +81,19 @@ impl BytecodeTokenMethods for Slt {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: crate::bytecode_parser::BytecodeInstruction
-    ) -> Result<(), String> {
+    ) -> Result<(), AtpError> {
         if instruction.op_code == Slt::default().get_opcode() {
             self.start_index = string_to_usize(&instruction.operands[1])?;
             self.end_index = string_to_usize(&instruction.operands[2])?;
             return Ok(());
-        } else {
-            return Err("An Atp Bytecode Parsing error ocurred: Invalid token".to_string());
         }
+        Err(
+            AtpError::new(
+                AtpErrorCode::BytecodeNotFound("".to_string()),
+                instruction.op_code.to_string(),
+                instruction.operands.join(" ")
+            )
+        )
     }
 
     fn token_to_bytecode_instruction(&self) -> crate::bytecode_parser::BytecodeInstruction {
