832a76b6aeeb0a227a2973cf96d285e9467ab8c9
redlintles <treelands54@gmail.com>
2025-07-08 22:22:28 -0300

    Adaptado text_parser para incluir o registro de erros via ErrorManager

--- src/text_parser/parser.rs
+++ src/text_parser/parser.rs
@@ -1,5 +1,15 @@
-use crate::{ token_data::TokenMethods, utils::errors::AtpError };
+use crate::{ token_data::TokenMethods, utils::errors::{ AtpError, ErrorManager } };
 
-pub fn parse_token(token: &dyn TokenMethods, input: &str) -> Result<String, AtpError> {
-    token.parse(input)
+pub fn parse_token(
+    token: &dyn TokenMethods,
+    input: &str,
+    error_manager: &mut ErrorManager
+) -> Result<String, AtpError> {
+    match token.parse(input) {
+        Ok(x) => Ok(x),
+        Err(e) => {
+            error_manager.add_error(e.clone());
+            Err(e)
+        }
+    }
 }
