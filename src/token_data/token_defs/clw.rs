87e94cee12e0b95ffb8e0b82b448463e04b0ff89
redlintles <treelands54@gmail.com>
2025-07-17 19:14:54 -0300

    Adicionado testes unitários em clw(Capitalize Last Word)

--- src/token_data/token_defs/clw.rs
+++ src/token_data/token_defs/clw.rs
@@ -72,3 +72,71 @@ impl BytecodeTokenMethods for Clw {
         BytecodeInstruction { op_code: Clw::default().get_opcode(), operands: [].to_vec() }
     }
 }
+
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod clw_tests {
+    use crate::{
+        token_data::{ token_defs::clw::Clw, TokenMethods },
+        utils::transforms::capitalize,
+    };
+
+    #[test]
+    fn test_capitalize_last_word() {
+        let random_text = random_string::generate(6, ('a'..'z').collect::<String>());
+
+        let mut token = Clw::default();
+
+        assert_eq!(
+            token.parse(&random_text),
+            Ok(capitalize(&random_text)),
+            "It supports random inputs"
+        );
+        assert_eq!(
+            token.parse("banana bananosa"),
+            Ok("banana Bananosa".to_string()),
+            "It supports expected inputs"
+        );
+        assert_eq!(
+            token.token_to_atp_line(),
+            "clw;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(token.get_string_repr(), "clw".to_string(), "get_string_repr works as expected");
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["clw".to_string()].to_vec()), Ok(_)),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn test_capitalize_last_word_bytecode() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Clw::default();
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x19,
+            operands: [].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x19, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
+    }
+}
