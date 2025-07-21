79ea70263221ea860cf66f3242be4383392d8d5e
redlintles <treelands54@gmail.com>
2025-07-21 20:44:46 -0300

    Adicionados testes unitários em CTS(Capitalize Single)

--- src/token_data/token_defs/cts.rs
+++ src/token_data/token_defs/cts.rs
@@ -22,10 +22,21 @@ impl TokenMethods for Cts {
         "cts".to_string()
     }
     fn parse(&self, input: &str) -> Result<String, AtpError> {
+        if !(0..input.chars().count()).contains(&self.index) {
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::IndexOutOfRange(
+                        "self.index does not exist in current input".to_string()
+                    ),
+                    self.token_to_atp_line(),
+                    input.to_string()
+                )
+            );
+        }
         let v = input.split_whitespace().collect::<Vec<_>>();
 
-        let i = match self.index > v.len() {
-            true => v.len() - 1,
+        let i = match self.index > v.iter().count() {
+            true => v.iter().count() - 1,
             false => self.index,
         };
 
@@ -70,8 +81,8 @@ impl BytecodeTokenMethods for Cts {
         &mut self,
         instruction: crate::bytecode_parser::BytecodeInstruction
     ) -> Result<(), AtpError> {
-        if instruction.op_code == Cts::default().get_opcode() {
-            self.index = string_to_usize(&instruction.operands[1])?;
+        if instruction.op_code == Cts::default().get_opcode() && !instruction.operands.is_empty() {
+            self.index = string_to_usize(&instruction.operands[0])?;
             return Ok(());
         }
         Err(
@@ -88,5 +99,69 @@ impl BytecodeTokenMethods for Cts {
             op_code: Cts::default().get_opcode(),
             operands: [self.index.to_string()].to_vec(),
         }
+    }
+}
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod cts_tests {
+    use crate::{ token_data::{ TokenMethods, token_defs::cts::Cts } };
+
+    #[test]
+    fn test_capitalize_single() {
+        let mut token = Cts::params(3);
+
+        assert_eq!(
+            token.parse("banana laranja vermelha azul"),
+            Ok("banana laranja vermelha Azul".to_string()),
+            "It works correctly with expected inputs"
+        );
+        assert!(
+            matches!(token.parse(""), Err(_)),
+            "It throws an error if the string does not have the current token index"
+        );
+
+        assert_eq!(
+            token.token_to_atp_line(),
+            "cts 3;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(token.get_string_repr(), "cts".to_string(), "get_string_repr works as expected");
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(
+                token.token_from_vec_params(["cts".to_string(), (3).to_string()].to_vec()),
+                Ok(_)
+            ),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+
+    #[test]
+    fn test_capitalize_single_bytecode() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Cts::params(3);
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x1d,
+            operands: [(3).to_string()].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x1d, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
     }
 }
