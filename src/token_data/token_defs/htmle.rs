d616b08d6560dc2c0e94167a82897606aa36cadf
redlintles <treelands54@gmail.com>
2025-07-22 20:59:25 -0300

    Adicionada documentação e testes unitários para HTMLE

--- src/token_data/token_defs/htmle.rs
+++ src/token_data/token_defs/htmle.rs
@@ -5,6 +5,21 @@ use crate::{ token_data::TokenMethods, utils::errors::{ AtpError, AtpErrorCode }
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeTokenMethods, BytecodeInstruction };
 
+/// HTMLE - HTML Escape
+///
+/// Escapes Special HTML Characters in `input` to HTML Entities
+/// So they can be rendered correctly later
+///
+/// # Example
+///
+/// ```rust
+/// use atp_project::token_data::{TokenMethods, token_defs::htmle::Htmle};
+///
+/// let token = Htmle::default();
+///
+/// assert_eq!(token.parse("<div>banana</div>"), Ok("&lt;div&gt;banana&lt;/div&gt;".to_string()));
+/// ```
+
 #[derive(Copy, Clone, Default)]
 pub struct Htmle {}
 
@@ -60,5 +75,66 @@ impl BytecodeTokenMethods for Htmle {
             op_code: Htmle::default().get_opcode(),
             operands: [].to_vec(),
         }
+    }
+}
+
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod htmle_tests {
+    use crate::token_data::{ TokenMethods, token_defs::htmle::Htmle };
+    #[test]
+    fn html_escape_test() {
+        let mut token = Htmle::default();
+
+        assert_eq!(
+            token.parse("<div>banana</div>"),
+            Ok("&lt;div&gt;banana&lt;/div&gt;".to_string()),
+            "It supports expected inputs!"
+        );
+        assert_eq!(
+            token.token_to_atp_line(),
+            "htmle;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(
+            token.get_string_repr(),
+            "htmle".to_string(),
+            "get_string_repr works as expected"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["htmle".to_string()].to_vec()), Ok(_)),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn html_escape_bytecode_test() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Htmle::default();
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x24,
+            operands: [].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x24, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
     }
 }
