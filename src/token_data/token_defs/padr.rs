8e8ebd05d5f975de677f6f49145eacb7d1cba023
redlintles <treelands54@gmail.com>
2025-07-23 19:16:21 -0300

    Adicionado testes unitários, documentação e corrigido erros de lógica em PADR(Pad Right)

--- src/token_data/token_defs/padr.rs
+++ src/token_data/token_defs/padr.rs
@@ -1,19 +1,38 @@
-use crate::{ token_data::TokenMethods, utils::errors::{ AtpError, AtpErrorCode } };
+use crate::{
+    token_data::TokenMethods,
+    utils::{ errors::{ AtpError, AtpErrorCode }, transforms::extend_string },
+};
 
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeTokenMethods, BytecodeInstruction };
-
+/// PADR - Pad Right
+///
+/// Repeats `text` characters until `max_len` is reached, and then insert the result at the end of `input`
+///
+/// See Also:
+///
+/// - [`Padr` - Pad Left](crate::token_data::token_defs::padr)
+///
+/// # Example:
+///
+/// ```rust
+/// use atp_project::token_data::{TokenMethods, token_defs::padr::Padr};
+///
+/// let token = Padr::params("xy", 7);
+///
+/// assert_eq!(token.parse("banana"), Ok("bananax".to_string()));
+/// ```
 #[derive(Clone, Default)]
 pub struct Padr {
     pub text: String,
-    pub times: usize,
+    pub max_len: usize,
 }
 
 impl Padr {
-    pub fn params(text: &str, times: usize) -> Self {
+    pub fn params(text: &str, max_len: usize) -> Self {
         Padr {
             text: text.to_string(),
-            times,
+            max_len,
         }
     }
 }
@@ -23,10 +42,16 @@ impl TokenMethods for Padr {
         "padr".to_string()
     }
     fn token_to_atp_line(&self) -> String {
-        "padr;\n".to_string()
+        format!("padr {} {};\n", self.text, self.max_len)
     }
     fn parse(&self, input: &str) -> Result<String, AtpError> {
-        let s = self.text.repeat(self.times);
+        let character_count = input.chars().count();
+
+        if character_count >= self.max_len {
+            return Ok(input.to_string());
+        }
+        let ml = self.max_len - character_count;
+        let s = extend_string(&self.text, ml);
 
         Ok(format!("{}{}", input, s))
     }
@@ -56,8 +81,8 @@ impl BytecodeTokenMethods for Padr {
         if instruction.op_code == self.get_opcode() {
             use crate::utils::transforms::string_to_usize;
 
-            self.text = instruction.operands[1].clone();
-            self.times = string_to_usize(&instruction.operands[2])?;
+            self.text = instruction.operands[0].clone();
+            self.max_len = string_to_usize(&instruction.operands[1])?;
             return Ok(());
         }
 
@@ -72,7 +97,68 @@ impl BytecodeTokenMethods for Padr {
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
         BytecodeInstruction {
             op_code: self.get_opcode(),
-            operands: [self.text.clone(), self.times.to_string()].to_vec(),
+            operands: [self.text.clone(), self.max_len.to_string()].to_vec(),
         }
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "test_access")]
+mod padr_tests {
+    use crate::token_data::{ TokenMethods, token_defs::padr::Padr };
+    #[test]
+    fn padr_tests() {
+        let mut token = Padr::params("xy", 7);
+        assert_eq!(token.parse("banana"), Ok("bananax".to_string()), "It supports expected inputs");
+
+        assert_eq!(
+            token.token_to_atp_line(),
+            "padr xy 7;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(
+            token.get_string_repr(),
+            "padr".to_string(),
+            "get_string_repr works as expected"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(
+                token.token_from_vec_params(
+                    ["padr".to_string(), "xy".to_string(), (7).to_string()].to_vec()
+                ),
+                Ok(_)
+            ),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn padr_bytecode_tests() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Padr::default();
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x30,
+            operands: ["xy".to_string(), (7).to_string()].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x30, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
     }
 }
