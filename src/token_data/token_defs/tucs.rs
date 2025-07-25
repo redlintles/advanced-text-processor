4c607ec213d92b68ccf53b0dffc22fd7de93bccb
redlintles <treelands54@gmail.com>
2025-07-24 22:55:56 -0300

    Adicionada documentação e testes unitários para o token TUCS(To Uppercase Single)

--- src/token_data/token_defs/tucs.rs
+++ src/token_data/token_defs/tucs.rs
@@ -6,6 +6,21 @@ use crate::{
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
 
+/// TUCS - To Uppercase Single
+///
+/// Uppercases a single character in `input` identified by `index`
+///
+/// # Example
+///
+/// ```rust
+/// use atp_project::token_data::{TokenMethods, token_defs::tucs::Tucs};
+///
+/// let token = Tucs::params(1);
+///
+/// assert_eq!(token.parse("banana"), Ok("bAnana".to_string()));
+///
+/// ```
+
 #[derive(Clone, Copy, Default)]
 pub struct Tucs {
     index: usize,
@@ -28,6 +43,17 @@ impl TokenMethods for Tucs {
         format!("tucs {};\n", self.index)
     }
     fn parse(&self, input: &str) -> Result<String, AtpError> {
+        if !(0..input.chars().count()).contains(&self.index) {
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::IndexOutOfRange(
+                        "Index does not exist in the splitted vec".to_string()
+                    ),
+                    self.token_to_atp_line(),
+                    input.to_string()
+                )
+            );
+        }
         let result: String = input
             .char_indices()
             .map(|(i, c)| {
@@ -60,8 +86,8 @@ impl BytecodeTokenMethods for Tucs {
         &mut self,
         instruction: BytecodeInstruction
     ) -> Result<(), AtpError> {
-        if instruction.op_code == Tucs::default().get_opcode() {
-            self.index = string_to_usize(&instruction.operands[1])?;
+        if instruction.op_code == Tucs::default().get_opcode() && instruction.operands.len() == 1 {
+            self.index = string_to_usize(&instruction.operands[0])?;
             return Ok(());
         }
 
@@ -79,5 +105,101 @@ impl BytecodeTokenMethods for Tucs {
             op_code: Tucs::default().get_opcode(),
             operands: [self.index.to_string()].to_vec(),
         }
+    }
+}
+
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod tucs_tests {
+    use crate::token_data::{ TokenMethods, token_defs::tucs::Tucs };
+    #[test]
+    fn to_lowercase_word_tests() {
+        let mut token = Tucs::params(1);
+
+        assert_eq!(token.parse("banana"), Ok("bAnana".to_string()));
+
+        assert!(
+            matches!(token.parse(""), Err(_)),
+            "It throws an error if start_index does not exists in input"
+        );
+
+        assert_eq!(
+            token.token_to_atp_line(),
+            "tucs 1;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+
+        assert_eq!(
+            token.get_string_repr(),
+            "tucs".to_string(),
+            "get_string_repr works as expected"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(
+                token.token_from_vec_params(["tucs".to_string(), "banana".to_string()].to_vec()),
+                Err(_)
+            ),
+            "It throws an error for invalid operands"
+        );
+        assert!(
+            matches!(
+                token.token_from_vec_params(["tucs".to_string(), (1).to_string()].to_vec()),
+                Ok(_)
+            ),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn to_lowercase_word_bytecode_tests() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Tucs::params(1);
+
+        let mut instruction = BytecodeInstruction {
+            op_code: 0x14,
+            operands: [(1).to_string()].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x14, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
+
+        instruction.operands = ["(".to_string(), (1).to_string()].to_vec();
+
+        assert!(
+            matches!(token.token_from_bytecode_instruction(instruction.clone()), Err(_)),
+            "It throws an error for invalid operands"
+        );
+
+        instruction.op_code = 0x01;
+        assert!(
+            matches!(token.token_from_bytecode_instruction(instruction.clone()), Err(_)),
+            "It throws an error for invalid op_code"
+        );
+        assert!(
+            matches!(
+                token.token_from_vec_params(
+                    ["tucs".to_string(), "(".to_string(), (1).to_string()].to_vec()
+                ),
+                Err(_)
+            ),
+            "It throws an error for invalid param vec"
+        );
     }
 }
