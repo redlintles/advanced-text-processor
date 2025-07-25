5feb772365ebdc1244083c5c63089482db9f7868
redlintles <treelands54@gmail.com>
2025-07-24 21:20:05 -0300

    Adicionadas documentação e testes unitários em SPLC(Split Characters)

--- src/token_data/token_defs/splc.rs
+++ src/token_data/token_defs/splc.rs
@@ -5,6 +5,20 @@ use crate::utils::errors::{ AtpError, AtpErrorCode };
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeTokenMethods, BytecodeInstruction };
 
+/// SPLC - Split Characters
+///
+/// Split `input` characters in a result whose chars are separed by spaces
+///
+/// # Example
+///
+/// ```rust
+/// use atp_project::token_data::{TokenMethods, token_defs::splc::Splc};
+///
+/// let token = Splc::default();
+///
+/// assert_eq!(token.parse("banana"), Ok("b a n a n a".to_string()));
+/// ```
+///
 #[derive(Clone, Copy, Default)]
 pub struct Splc {}
 
@@ -65,5 +79,70 @@ impl BytecodeTokenMethods for Splc {
             op_code: Splc::default().get_opcode(),
             operands: [].to_vec(),
         }
+    }
+}
+
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod splc_tests {
+    use crate::token_data::{ TokenMethods, token_defs::splc::Splc };
+    #[test]
+    fn split_characters_tests() {
+        let mut token = Splc::default();
+        assert_eq!(
+            token.parse("banana"),
+            Ok("b a n a n a".to_string()),
+            "It supports expected inputs"
+        );
+        assert_eq!(
+            token.token_to_atp_line(),
+            "splc;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(
+            token.get_string_repr(),
+            "splc".to_string(),
+            "get_string_repr works as expected"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["splc".to_string()].to_vec()), Ok(_)),
+            "It does not throws an error for valid vec_params"
+        );
+        assert_eq!(
+            token.parse("banana"),
+            Ok("b a n a n a".to_string()),
+            "It works correctly after re-parsing"
+        );
+    }
+
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn split_characters_bytecode_tests() {
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Splc::default();
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x23,
+            operands: [].to_vec(),
+        };
+
+        assert_eq!(token.get_opcode(), 0x23, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
     }
 }
