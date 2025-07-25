8eaf033445a35cad3fa2f10ad71a7469c24c717f
redlintles <treelands54@gmail.com>
2025-07-24 23:03:39 -0300

    Adicionada documentação e testes unitários para URLD(URL Decode)

--- src/token_data/token_defs/urld.rs
+++ src/token_data/token_defs/urld.rs
@@ -2,6 +2,20 @@ use crate::{ token_data::TokenMethods, utils::errors::{ AtpError, AtpErrorCode }
 
 #[cfg(feature = "bytecode")]
 use crate::bytecode_parser::{ BytecodeTokenMethods, BytecodeInstruction };
+/// URLD - URL Decode
+///
+/// Decodes `input` from the URL Encoding Format
+///
+/// # Example
+///
+/// ```rust
+/// use atp_project::token_data::{TokenMethods, token_defs::urld::Urld};
+///
+/// let token = Urld::default();
+///
+/// assert_eq!(token.parse("banana%20laranja"), Ok("banana laranja".to_string()));
+/// ```
+///
 
 #[derive(Copy, Clone, Default)]
 pub struct Urld {}
@@ -69,5 +83,66 @@ impl BytecodeTokenMethods for Urld {
             op_code: Urld::default().get_opcode(),
             operands: [].to_vec(),
         }
+    }
+}
+
+#[cfg(feature = "test_access")]
+#[cfg(test)]
+mod urld_tests {
+    use crate::token_data::{ token_defs::urld::Urld, TokenMethods };
+    #[test]
+    fn test_trim_right_side() {
+        let mut token = Urld::default();
+
+        assert_eq!(
+            token.parse("banana%20laranja"),
+            Ok("banana laranja".to_string()),
+            "It supports expected inputs"
+        );
+        assert_eq!(
+            token.token_to_atp_line(),
+            "urld;\n".to_string(),
+            "conversion to atp_line works correctly"
+        );
+        assert_eq!(
+            token.get_string_repr(),
+            "urld".to_string(),
+            "get_string_repr works as expected"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["tks".to_string()].to_vec()), Err(_)),
+            "It throws an error for invalid vec_params"
+        );
+        assert!(
+            matches!(token.token_from_vec_params(["urld".to_string()].to_vec()), Ok(_)),
+            "It does not throws an error for valid vec_params"
+        );
+    }
+
+    #[cfg(feature = "bytecode")]
+    #[test]
+    fn test_bytecode_trim_right_side() {
+        use crate::token_data::{ token_defs::urld::Urld };
+        use crate::bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods };
+
+        let mut token = Urld::default();
+
+        let instruction = BytecodeInstruction {
+            op_code: 0x21,
+            operands: [].to_vec(),
+        };
+        assert_eq!(token.get_opcode(), 0x21, "get_opcode does not disrepect ATP token mapping");
+
+        assert_eq!(
+            token.token_from_bytecode_instruction(instruction.clone()),
+            Ok(()),
+            "Parsing from bytecode to token works correctly!"
+        );
+
+        assert_eq!(
+            token.token_to_bytecode_instruction(),
+            instruction,
+            "Conversion to bytecode instruction works perfectly!"
+        );
     }
 }
