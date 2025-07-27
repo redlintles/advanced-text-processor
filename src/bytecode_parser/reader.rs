903996f6f6855884b46ec1589081515014c44e18
redlintles <treelands54@gmail.com>
2025-07-27 19:52:31 -0300

    Melhorado código em bytecode_parser/reader.rs, minimizando repetição de código, e tornado mais versátil

--- src/bytecode_parser/reader.rs
+++ src/bytecode_parser/reader.rs
@@ -9,6 +9,108 @@ use crate::utils::{
 
 use super::{ BytecodeInstruction, BytecodeTokenMethods };
 
+pub fn read_bytecode_from_text(
+    token_string: &str
+) -> Result<Box<dyn BytecodeTokenMethods>, AtpError> {
+    let chunks = match shell_words::split(&token_string) {
+        Ok(x) => x,
+        Err(_) => {
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::TextParsingError(
+                        "An ATP Parsing error ocurred: Error splitting file line".to_string()
+                    ),
+                    "shell words split".to_string(),
+                    token_string.to_string()
+                )
+            );
+        }
+    };
+
+    let brute_op_code = match chunks.first() {
+        Some(x) => x,
+        None => {
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
+                        "Failed Parsing op_code".to_string()
+                    ),
+                    "".to_string(),
+                    chunks.join(" ")
+                )
+            );
+        }
+    };
+
+    let parsed_op_code = match token_from_hex_string(&brute_op_code) {
+        Some(x) => x,
+        None => {
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
+                        "Failed Parsing op_code".to_string()
+                    ),
+                    "".to_string(),
+                    chunks.join(" ")
+                )
+            );
+        }
+    };
+
+    let instruction = BytecodeInstruction {
+        op_code: parsed_op_code,
+        operands: chunks,
+    };
+
+    let op_code_map = get_mapping_bytecode_to_token();
+
+    let mapped_token = match op_code_map.get(&(parsed_op_code as u8)) {
+        Some(x) => x,
+        None => {
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::TokenNotFound(
+                        "Token not recognized".to_string()
+                    ),
+                    "".to_string(),
+                    parsed_op_code.to_string()
+                )
+            );
+        }
+    };
+
+    let mut new_token = mapped_token();
+
+    match new_token.token_from_bytecode_instruction(instruction) {
+        Ok(()) => {
+            return Ok(new_token);
+        }
+        Err(_) => {
+            return Err(
+                AtpError::new(
+                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
+                        "Token not recognized".to_string()
+                    ),
+                    "".to_string(),
+                    parsed_op_code.to_string()
+                )
+            );
+        }
+    }
+}
+
+pub fn read_bytecode_from_text_vec(
+    tokens: Vec<String>
+) -> Result<Vec<Box<dyn BytecodeTokenMethods>>, AtpError> {
+    let mut result: Vec<Box<dyn BytecodeTokenMethods>> = Vec::new();
+
+    for line_text in tokens.iter() {
+        result.push(read_bytecode_from_text(&line_text)?);
+    }
+
+    Ok(result)
+}
+
 pub fn read_bytecode_from_file(
     path: &Path
 ) -> Result<Vec<Box<dyn BytecodeTokenMethods>>, AtpError> {
@@ -48,89 +150,7 @@ pub fn read_bytecode_from_file(
             }
         };
 
-        let chunks = match shell_words::split(&line_text) {
-            Ok(x) => x,
-            Err(_) => {
-                return Err(
-                    AtpError::new(
-                        AtpErrorCode::TextParsingError(
-                            "An ATP Parsing error ocurred: Error splitting file line".to_string()
-                        ),
-                        "shell words split".to_string(),
-                        line_text.to_string()
-                    )
-                );
-            }
-        };
-
-        let brute_op_code = match chunks.first() {
-            Some(x) => x,
-            None => {
-                return Err(
-                    AtpError::new(
-                        crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                            "Failed Parsing op_code".to_string()
-                        ),
-                        "".to_string(),
-                        chunks.join(" ")
-                    )
-                );
-            }
-        };
-
-        let parsed_op_code = match token_from_hex_string(&brute_op_code) {
-            Some(x) => x,
-            None => {
-                return Err(
-                    AtpError::new(
-                        crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                            "Failed Parsing op_code".to_string()
-                        ),
-                        "".to_string(),
-                        chunks.join(" ")
-                    )
-                );
-            }
-        };
-
-        let instruction = BytecodeInstruction {
-            op_code: parsed_op_code,
-            operands: chunks,
-        };
-
-        let op_code_map = get_mapping_bytecode_to_token();
-
-        let mapped_token = match op_code_map.get(&(parsed_op_code as u8)) {
-            Some(x) => x,
-            None => {
-                return Err(
-                    AtpError::new(
-                        crate::utils::errors::AtpErrorCode::TokenNotFound(
-                            "Token not recognized".to_string()
-                        ),
-                        "".to_string(),
-                        parsed_op_code.to_string()
-                    )
-                );
-            }
-        };
-
-        let mut new_token = mapped_token();
-
-        match new_token.token_from_bytecode_instruction(instruction) {
-            Ok(()) => result.push(new_token),
-            Err(_) => {
-                return Err(
-                    AtpError::new(
-                        crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                            "Token not recognized".to_string()
-                        ),
-                        "".to_string(),
-                        parsed_op_code.to_string()
-                    )
-                );
-            }
-        }
+        result.push(read_bytecode_from_text(&line_text)?);
     }
 
     Ok(result)
