85e6a7e429cb1649adb81753f7397c610b87b270
redlintles <treelands54@gmail.com>
2025-07-27 21:04:13 -0300

    Adicionados métodos da feature bytecode ao ATPProcessorJS da  FFI de javascript

--- src/ffi/javascript/main/processor.rs
+++ src/ffi/javascript/main/processor.rs
@@ -3,7 +3,8 @@ use std::path::Path;
 use napi_derive::napi;
 
 use crate::{
-    builder::atp_processor::{ AtpProcessor, AtpProcessorMethods },
+    builder::atp_processor::{ AtpProcessor, AtpProcessorBytecodeMethods, AtpProcessorMethods },
+    bytecode_parser::reader::read_bytecode_from_text,
     text_parser::reader::{ read_from_text, read_from_text_vec },
 };
 
@@ -59,5 +60,28 @@ impl ATPProcessorJS {
 
     pub fn process_all_with_debug(&mut self, id: String, input: String) -> napi::Result<String> {
         Ok(self.inner.process_all_with_debug(&id, &input)?)
+    }
+
+    pub fn write_to_bytecode_file(&mut self, id: String, path: String) -> napi::Result<()> {
+        Ok(self.inner.write_to_bytecode_file(&id, Path::new(&path))?)
+    }
+    pub fn read_from_bytecode_file(&mut self, path: String) -> napi::Result<String> {
+        Ok(self.inner.read_from_bytecode_file(Path::new(&path))?)
+    }
+
+    pub fn process_all_bytecode_with_debug(
+        &mut self,
+        id: String,
+        input: String
+    ) -> napi::Result<String> {
+        Ok(self.inner.process_all_bytecode_with_debug(&id, &input)?)
+    }
+    pub fn process_single_bytecode_with_debug(
+        &mut self,
+        token: String,
+        input: String
+    ) -> napi::Result<String> {
+        let parsed_token = read_bytecode_from_text(&token)?;
+        Ok(self.inner.process_single_bytecode_with_debug(parsed_token, &input)?)
     }
 }
