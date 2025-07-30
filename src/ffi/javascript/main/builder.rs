b6f627fa7005566f61184d454ec777c420c5fd27
redlintles <treelands54@gmail.com>
2025-07-29 23:01:19 -0300

    Adicionados todos os métodos de tokens não parametrizados à ffi/javascript/builder

--- src/ffi/javascript/main/builder.rs
+++ src/ffi/javascript/main/builder.rs
@@ -18,6 +18,7 @@ impl AtpBuilderJS {
             inner: AtpBuilder::new(),
         }
     }
+
     #[napi]
     pub fn build(&self) -> napi::Result<(ATPProcessorJS, String)> {
         let (p, i) = self.inner.build();
@@ -28,5 +29,93 @@ impl AtpBuilderJS {
         let id = processor.add_transform(text_vec)?;
 
         Ok((processor, id))
+    }
+}
+
+#[napi]
+impl AtpBuilderJS {
+    #[napi]
+    pub fn trim_both(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().trim_both(),
+        }
+    }
+    #[napi]
+    pub fn trim_left(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().trim_left(),
+        }
+    }
+    #[napi]
+    pub fn trim_right(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().trim_right(),
+        }
+    }
+    #[napi]
+    pub fn delete_first(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().delete_first(),
+        }
+    }
+    #[napi]
+    pub fn delete_last(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().delete_last(),
+        }
+    }
+    #[napi]
+    pub fn split_characters(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().split_characters(),
+        }
+    }
+    #[napi]
+    pub fn to_uppercase_all(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().to_uppercase_all(),
+        }
+    }
+    #[napi]
+    pub fn to_lowercase_all(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().to_lowercase_all(),
+        }
+    }
+    #[napi]
+    pub fn to_url_encoded(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().to_url_encoded(),
+        }
+    }
+    #[napi]
+    pub fn to_url_decoded(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().to_url_decoded(),
+        }
+    }
+    #[napi]
+    pub fn join_to_camel_case(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().join_to_camel_case(),
+        }
+    }
+    #[napi]
+    pub fn join_to_pascal_case(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().join_to_pascal_case(),
+        }
+    }
+    #[napi]
+    pub fn join_to_kebab_case(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().join_to_kebab_case(),
+        }
+    }
+    #[napi]
+    pub fn join_to_snake_case(&mut self) -> Self {
+        AtpBuilderJS {
+            inner: self.inner.clone().join_to_snake_case(),
+        }
     }
 }
