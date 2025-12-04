efa6ee6be310c858b8615faf0c4a46e52c7d4e27
redlintles <treelands54@gmail.com>
2025-12-04 18:56:24 -0300

    Modificado DLF para parsing de bytecode

--- src/tokens/transforms/dlf.rs
+++ src/tokens/transforms/dlf.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use crate::{ tokens::TokenMethods, utils::errors::{ AtpError, AtpErrorCode } };
 
 #[cfg(feature = "bytecode")]
-use crate::bytecode::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::bytecode::{ BytecodeTokenMethods };
 /// DLF - Delete First
 ///
 /// Deletes the first character of `input`
@@ -52,31 +52,29 @@ impl TokenMethods for Dlf {
 }
 #[cfg(feature = "bytecode")]
 impl BytecodeTokenMethods for Dlf {
-    fn token_from_bytecode_instruction(
-        &mut self,
-        instruction: BytecodeInstruction
-    ) -> Result<(), AtpError> {
-        if instruction.op_code == Dlf::default().get_opcode() {
+    fn get_opcode(&self) -> u8 {
+        0x03
+    }
+
+    fn token_from_bytecode_instruction(&mut self, instruction: Vec<u8>) -> Result<(), AtpError> {
+        if instruction[0] == Dlf::default().get_opcode() {
             return Ok(());
         }
 
         Err(
             AtpError::new(
-                AtpErrorCode::BytecodeNotFound("".into()),
-                instruction.op_code.to_string(),
-                instruction.operands.join(" ")
+                AtpErrorCode::BytecodeNotFound("Invalid Parser for this token".into()),
+                instruction[0].to_string(),
+                instruction
+                    .iter()
+                    .map(|b| b.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ")
             )
         )
     }
-
-    fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
-        BytecodeInstruction {
-            op_code: Dlf::default().get_opcode(),
-            operands: [].to_vec(),
-        }
-    }
-    fn get_opcode(&self) -> u8 {
-        0x03
+    fn token_to_bytecode_instruction(&self) -> Vec<u8> {
+        vec![Dlf::default().get_opcode(), 0]
     }
 }
 
@@ -120,14 +118,11 @@ mod dlf_tests {
     #[cfg(feature = "bytecode")]
     #[test]
     fn test_delete_first_bytecode() {
-        use crate::bytecode::{ BytecodeInstruction, BytecodeTokenMethods };
+        use crate::bytecode::{ BytecodeTokenMethods };
 
         let mut token = Dlf::default();
 
-        let instruction = BytecodeInstruction {
-            op_code: 0x03,
-            operands: [].to_vec(),
-        };
+        let instruction = vec![0x03, 0];
 
         assert_eq!(token.get_opcode(), 0x03, "get_opcode does not disrepect ATP token mapping");
 
