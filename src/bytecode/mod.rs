13868cb06a731bb7c8d22077014b6331bb944b12
redlintles <treelands54@gmail.com>
2025-12-03 23:11:25 -0300

    Removida struct BytecodeInstruction

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -4,31 +4,10 @@ pub mod writer;
 pub mod reader;
 pub mod transformer;
 pub mod parser;
-#[derive(Clone, Default, PartialEq, Debug)]
-pub struct BytecodeInstruction {
-    pub op_code: u8,
-    pub operands: Vec<String>,
-}
-
-impl BytecodeInstruction {
-    pub fn to_bytecode_line(&self) -> String {
-        let mut result = format!("{:#04x}", self.op_code as u8);
-        for operand in self.operands.iter() {
-            result = format!("{} {}", result, operand);
-        }
-
-        result.push_str("\n");
-
-        result
-    }
-}
 
 pub trait BytecodeTokenMethods: TokenMethods {
-    fn token_from_bytecode_instruction(
-        &mut self,
-        instruction: BytecodeInstruction
-    ) -> Result<(), AtpError>;
-    fn token_to_bytecode_instruction(&self) -> BytecodeInstruction;
+    fn token_from_bytecode_instruction(&mut self, instruction: Vec<u8>) -> Result<(), AtpError>;
+    fn token_to_bytecode_instruction(&self) -> Vec<u8>;
 
     fn get_opcode(&self) -> u8;
 }
