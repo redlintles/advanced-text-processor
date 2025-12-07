3da0014f836b58e592fad6d03caf202282248b85
redlintles <treelands54@gmail.com>
2025-12-06 22:44:15 -0300

    Modificado TUCW para atender à nova trait BytecodeTokenMethods

--- src/tokens/transforms/tucw.rs
+++ src/tokens/transforms/tucw.rs
@@ -9,7 +9,7 @@ use crate::{
     },
 };
 #[cfg(feature = "bytecode")]
-use crate::bytecode::{ BytecodeInstruction, BytecodeTokenMethods };
+use crate::{ bytecode::BytecodeTokenMethods, utils::bytecode_utils::AtpParamTypes };
 /// TUCW - To Uppercase Word
 ///
 /// Uppercase a single word of string
@@ -80,30 +80,65 @@ impl BytecodeTokenMethods for Tucw {
     fn get_opcode(&self) -> u8 {
         0x2a
     }
-    fn token_from_bytecode_instruction(
-        &mut self,
-        instruction: BytecodeInstruction
-    ) -> Result<(), AtpError> {
-        check_vec_len(&instruction.operands, 1)?;
-        if instruction.op_code == Tucw::default().get_opcode() {
-            self.index = string_to_usize(&instruction.operands[0])?;
 
-            return Ok(());
+    fn from_params(&mut self, instruction: Vec<AtpParamTypes>) -> Result<(), AtpError> {
+        if instruction.len() != 1 {
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::BytecodeNotFound("Invalid Parser for this token".into()),
+                    "",
+                    ""
+                )
+            );
         }
 
-        Err(
-            AtpError::new(
-                AtpErrorCode::BytecodeNotFound("Invalid Parser for this token".into()),
-                instruction.op_code.to_string(),
-                instruction.operands.join(" ")
-            )
-        )
+        match &instruction[0] {
+            AtpParamTypes::Usize(payload) => {
+                self.index = payload.clone();
+                return Ok(());
+            }
+            _ => {
+                Err(
+                    AtpError::new(
+                        AtpErrorCode::InvalidParameters(
+                            "This token takes a single usize as argument".into()
+                        ),
+                        "",
+                        ""
+                    )
+                )
+            }
+        }
     }
-    fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
-        BytecodeInstruction {
-            op_code: Tucw::default().get_opcode(),
-            operands: [self.index.to_string()].to_vec(),
-        }
+
+    fn to_bytecode(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+
+        let instruction_type: u32 = self.get_opcode() as u32;
+
+        let first_param_type: u32 = 0x02;
+        let first_param_payload = (self.index as u32).to_be_bytes();
+        let first_param_payload_size: u32 = first_param_payload.len() as u32;
+
+        let first_param_total_size: u64 = 8 + 4 + 4 + (first_param_payload_size as u64);
+        let instruction_total_size: u64 = 8 + 4 + 1 + first_param_total_size;
+
+        // Instruction Total Size
+        result.extend_from_slice(&instruction_total_size.to_be_bytes());
+        // Instruction Type
+        result.extend_from_slice(&instruction_type.to_be_bytes());
+        // Param Count
+        result.push(1);
+        // First Param Total Size
+        result.extend_from_slice(&first_param_total_size.to_be_bytes());
+        // First Param Type
+        result.extend_from_slice(&first_param_type.to_be_bytes());
+        // First Param Payload Size
+        result.extend_from_slice(&first_param_payload_size.to_be_bytes());
+        // First Param Payload
+        result.extend_from_slice(&first_param_payload);
+
+        result
     }
 }
 
@@ -156,49 +191,43 @@ mod tucw_tests {
     #[cfg(feature = "bytecode")]
     #[test]
     fn to_uppercase_word_bytecode_tests() {
-        use crate::bytecode::{ BytecodeInstruction, BytecodeTokenMethods };
+        use crate::{ bytecode::BytecodeTokenMethods, utils::bytecode_utils::AtpParamTypes };
 
-        let mut token = Tucw::params(1);
+        let mut token = Tucw::params(3);
 
-        let mut instruction = BytecodeInstruction {
-            op_code: 0x2a,
-            operands: [(1).to_string()].to_vec(),
-        };
+        let instruction: Vec<AtpParamTypes> = vec![AtpParamTypes::Usize(3)];
 
         assert_eq!(token.get_opcode(), 0x2a, "get_opcode does not disrepect ATP token mapping");
 
         assert_eq!(
-            token.token_from_bytecode_instruction(instruction.clone()),
+            token.from_params(instruction),
             Ok(()),
             "Parsing from bytecode to token works correctly!"
         );
 
+        let first_param_type: u32 = 0x02;
+        let first_param_payload = vec![0x03];
+        let first_param_payload_size = first_param_payload.len() as u32;
+        let first_param_total_size: u64 = 8 + 4 + 4 + (first_param_payload_size as u64);
+
+        let instruction_type: u32 = 0x2a;
+        let param_count: u8 = 0x01;
+
+        let instruction_total_size: u64 = 8 + 4 + 1 + first_param_total_size;
+
+        let mut expected_output: Vec<u8> = vec![];
+
+        expected_output.extend_from_slice(&instruction_total_size.to_be_bytes());
+        expected_output.extend_from_slice(&instruction_type.to_be_bytes());
+        expected_output.push(param_count);
+        expected_output.extend_from_slice(&first_param_total_size.to_be_bytes());
+        expected_output.extend_from_slice(&first_param_type.to_be_bytes());
+        expected_output.extend_from_slice(&first_param_payload_size.to_be_bytes());
+        expected_output.extend_from_slice(&first_param_payload);
         assert_eq!(
-            token.token_to_bytecode_instruction(),
-            instruction,
+            token.to_bytecode(),
+            expected_output,
             "Conversion to bytecode instruction works perfectly!"
-        );
-
-        instruction.operands = ["(".to_string(), (1).to_string()].to_vec();
-
-        assert!(
-            matches!(token.token_from_bytecode_instruction(instruction.clone()), Err(_)),
-            "It throws an error for invalid operands"
-        );
-
-        instruction.op_code = 0x01;
-        assert!(
-            matches!(token.token_from_bytecode_instruction(instruction.clone()), Err(_)),
-            "It throws an error for invalid op_code"
-        );
-        assert!(
-            matches!(
-                token.from_vec_params(
-                    ["tucw".to_string(), "(".to_string(), (1).to_string()].to_vec()
-                ),
-                Err(_)
-            ),
-            "It throws an error for invalid param vec"
         );
     }
 }
