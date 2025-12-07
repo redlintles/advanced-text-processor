640aa64dd9ac72ed04cc8c196449b119010502f4
redlintles <treelands54@gmail.com>
2025-12-06 22:42:28 -0300

    Modificado RMWS para atender à nova trait BytecodeTokenMethods

--- src/tokens/transforms/rmws.rs
+++ src/tokens/transforms/rmws.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use crate::{ tokens::TokenMethods, utils::errors::{ AtpError, AtpErrorCode } };
 
 #[cfg(feature = "bytecode")]
-use crate::bytecode::{ BytecodeTokenMethods };
+use crate::{ bytecode::BytecodeTokenMethods, utils::bytecode_utils::AtpParamTypes };
 
 /// RMWS - Remove Whitespace
 ///
@@ -53,26 +53,33 @@ impl BytecodeTokenMethods for Rmws {
     }
 
     fn from_params(&mut self, instruction: Vec<AtpParamTypes>) -> Result<(), AtpError> {
-        if instruction[0] == Rmws::default().get_opcode() {
+        if instruction.len() == 0 {
             return Ok(());
+        } else {
+            return Err(
+                AtpError::new(
+                    AtpErrorCode::BytecodeNotFound("Invalid Parser for this token".into()),
+                    "",
+                    ""
+                )
+            );
         }
-
-        Err(
-            AtpError::new(
-                AtpErrorCode::BytecodeNotFound("Invalid Parser for this token".into()),
-                instruction[0].to_string(),
-                instruction
-                    .iter()
-                    .map(|b| b.to_string())
-                    .collect::<Vec<String>>()
-                    .join(" ")
-            )
-        )
     }
-    fn token_to_bytecode_instruction(&self) -> Vec<u8> {
-        vec![Rmws::default().get_opcode(), 0]
+    fn to_bytecode(&self) -> Vec<u8> {
+        let opcode = self.get_opcode();
+
+        let size: u16 = 13; // tamanho total, incluindo header
+        let size_bytes = size.to_be_bytes(); // big-endian
+
+        let mut v = Vec::with_capacity(13);
+        v.push(opcode);
+        v.extend_from_slice(&size_bytes);
+        v.extend_from_slice(&[0u8; 10]); // payload vazio/padding
+
+        v
     }
 }
+
 #[cfg(feature = "test_access")]
 #[cfg(test)]
 mod rmws_tests {
@@ -108,23 +115,40 @@ mod rmws_tests {
     #[cfg(feature = "bytecode")]
     #[test]
     fn remove_whitespace_bytecode_tests() {
-        use crate::bytecode::{ BytecodeTokenMethods };
+        use crate::{ bytecode::BytecodeTokenMethods, utils::bytecode_utils::AtpParamTypes };
 
         let mut token = Rmws::default();
 
-        let instruction = vec![0x31, 0];
+        let instruction: Vec<AtpParamTypes> = vec![];
 
         assert_eq!(token.get_opcode(), 0x31, "get_opcode does not disrepect ATP token mapping");
 
         assert_eq!(
-            token.token_from_bytecode_instruction(instruction.clone()),
+            token.from_params(instruction),
             Ok(()),
             "Parsing from bytecode to token works correctly!"
         );
 
         assert_eq!(
-            token.token_to_bytecode_instruction(),
-            instruction,
+            token.to_bytecode(),
+            vec![
+                // Instruction total size
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x0d,
+                // Instruction Type
+                0x00,
+                0x00,
+                0x00,
+                0x31,
+                // Param Count
+                0x00
+            ],
             "Conversion to bytecode instruction works perfectly!"
         );
     }
