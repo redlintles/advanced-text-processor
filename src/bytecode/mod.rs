c96add8b826527b1bf87e2d200042cfa400142f6
redlintles <treelands54@gmail.com>
2025-12-20 18:49:02 -0300

    Removida trait BytecodeTokenMethods em definitivo

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -5,9 +5,9 @@ pub mod reader;
 pub mod transformer;
 pub mod parser;
 
-pub trait BytecodeTokenMethods: TokenMethods {
-    fn from_params(&mut self, instruction: &Vec<AtpParamTypes>) -> Result<(), AtpError>;
-    fn to_bytecode(&self) -> Vec<u8>;
+// pub trait BytecodeTokenMethods: TokenMethods {
+//     fn from_params(&mut self, instruction: &Vec<AtpParamTypes>) -> Result<(), AtpError>;
+//     fn to_bytecode(&self) -> Vec<u8>;
 
-    fn get_opcode(&self) -> u32;
-}
+//     fn get_opcode(&self) -> u32;
+// }
