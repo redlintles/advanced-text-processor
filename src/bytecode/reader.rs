bb5deb09177f9e9b19b81568f6f849a8f24ffbb6
redlintles <treelands54@gmail.com>
2025-12-20 16:42:41 -0300

    Reescrita função para ler a nova versão do bytecode

--- src/bytecode/reader.rs
+++ src/bytecode/reader.rs
@@ -1,115 +1,13 @@
-use std::{ fs::OpenOptions, io::{ BufRead, BufReader }, path::Path };
+use std::{ fs::OpenOptions, io::{ BufReader, Read }, path::Path };
 
 use crate::utils::{
+    params::AtpParamTypes,
     errors::{ AtpError, AtpErrorCode },
     mapping::get_mapping_bytecode_to_token,
-    transforms::token_from_hex_string,
     validations::check_file_path,
 };
 
-use super::{ BytecodeInstruction, BytecodeTokenMethods };
-
-pub fn read_bytecode_from_text(
-    token_string: &str
-) -> Result<Box<dyn BytecodeTokenMethods>, AtpError> {
-    let chunks = match shell_words::split(&token_string) {
-        Ok(x) => x,
-        Err(_) => {
-            return Err(
-                AtpError::new(
-                    AtpErrorCode::TextParsingError(
-                        "An ATP Parsing error ocurred: Error splitting file line".into()
-                    ),
-                    "shell words split",
-                    token_string.to_string()
-                )
-            );
-        }
-    };
-
-    let brute_op_code = match chunks.first() {
-        Some(x) => x,
-        None => {
-            return Err(
-                AtpError::new(
-                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                        "Failed Parsing op_code".into()
-                    ),
-                    "",
-                    chunks.join(" ")
-                )
-            );
-        }
-    };
-
-    let parsed_op_code = match token_from_hex_string(&brute_op_code) {
-        Some(x) => x,
-        None => {
-            return Err(
-                AtpError::new(
-                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                        "Failed Parsing op_code".into()
-                    ),
-                    "",
-                    chunks.join(" ")
-                )
-            );
-        }
-    };
-
-    let instruction = BytecodeInstruction {
-        op_code: parsed_op_code,
-        operands: chunks,
-    };
-
-    let op_code_map = get_mapping_bytecode_to_token();
-
-    let mapped_token = match op_code_map.get(&(parsed_op_code as u8)) {
-        Some(x) => x,
-        None => {
-            return Err(
-                AtpError::new(
-                    crate::utils::errors::AtpErrorCode::TokenNotFound(
-                        "Token not recognized".into()
-                    ),
-                    "",
-                    parsed_op_code.to_string()
-                )
-            );
-        }
-    };
-
-    let mut new_token = mapped_token();
-
-    match new_token.token_from_bytecode_instruction(instruction) {
-        Ok(()) => {
-            return Ok(new_token);
-        }
-        Err(_) => {
-            return Err(
-                AtpError::new(
-                    crate::utils::errors::AtpErrorCode::BytecodeParsingError(
-                        "Token not recognized".into()
-                    ),
-                    "",
-                    parsed_op_code.to_string()
-                )
-            );
-        }
-    }
-}
-
-pub fn read_bytecode_from_text_vec(
-    tokens: Vec<String>
-) -> Result<Vec<Box<dyn BytecodeTokenMethods>>, AtpError> {
-    let mut result: Vec<Box<dyn BytecodeTokenMethods>> = Vec::new();
-
-    for line_text in tokens.iter() {
-        result.push(read_bytecode_from_text(&line_text)?);
-    }
-
-    Ok(result)
-}
+use super::{ BytecodeTokenMethods };
 
 pub fn read_bytecode_from_file(
     path: &Path
@@ -132,25 +30,140 @@ pub fn read_bytecode_from_file(
         }
     };
 
-    let reader = BufReader::new(file);
+    let mut reader = BufReader::new(file);
 
-    for line in reader.lines() {
-        let line_text = match line {
-            Ok(x) => x,
-            Err(_) => {
-                return Err(
+    let mut magic_number = [0u8; 8];
+    let expected_magic_number: [u8; 8] = [38, 235, 245, 8, 244, 137, 1, 179];
+
+    reader
+        .read_exact(&mut magic_number)
+        .map_err(|e|
+            AtpError::new(
+                AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                "read_bytecode_from_file",
+                e.to_string()
+            )
+        )?;
+
+    if magic_number != expected_magic_number {
+        return Err(
+            AtpError::new(
+                AtpErrorCode::FileReadingError("Incompatible Magic Number with ATP".into()),
+                "bytecode reader",
+                ""
+            )
+        );
+    }
+
+    let mut protocol_version_bytes: [u8; 8] = [0u8; 8];
+    reader
+        .read_exact(&mut protocol_version_bytes)
+        .map_err(|e|
+            AtpError::new(
+                AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                "read_bytecode_from_file",
+                e.to_string()
+            )
+        )?;
+
+    let protocol_version = u64::from_be_bytes(protocol_version_bytes);
+
+    let mut instruction_count_bytes = [0u8; 4];
+    reader
+        .read_exact(&mut instruction_count_bytes)
+        .map_err(|e|
+            AtpError::new(
+                AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                "read_bytecode_from_file",
+                e.to_string()
+            )
+        )?;
+
+    let instruction_count = u32::from_be_bytes(instruction_count_bytes);
+
+    if protocol_version == 1 {
+        for _ in 0..instruction_count {
+            // Tipo da instrução
+            let mut instruction_type_bytes = [0u8; 4];
+            reader
+                .read_exact(&mut instruction_type_bytes)
+                .map_err(|e|
                     AtpError::new(
-                        crate::utils::errors::AtpErrorCode::FileReadingError(
-                            "Failed reading file line".into()
-                        ),
-                        "",
-                        ""
+                        AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                        "read_bytecode_from_file",
+                        e.to_string()
                     )
-                );
-            }
-        };
+                )?;
+            let instruction_type = u32::from_be_bytes(instruction_type_bytes);
 
-        result.push(read_bytecode_from_text(&line_text)?);
+            // Quantidade de parâmetros da instrução
+
+            let mut instruction_param_count_bytes = [0u8; 1];
+
+            reader
+                .read_exact(&mut instruction_param_count_bytes)
+                .map_err(|e|
+                    AtpError::new(
+                        AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                        "read_bytecode_from_file",
+                        e.to_string()
+                    )
+                )?;
+
+            let instruction_param_count = u8::from_be_bytes(instruction_param_count_bytes);
+
+            let mut params: Vec<AtpParamTypes> = Vec::new();
+
+            for _ in 0..instruction_param_count {
+                let mut param_total_size_bytes = [0u8; 8];
+                reader
+                    .read_exact(&mut param_total_size_bytes)
+                    .map_err(|e|
+                        AtpError::new(
+                            AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                            "read_bytecode_from_file",
+                            e.to_string()
+                        )
+                    )?;
+                let param_total_size = u64::from_be_bytes(param_total_size_bytes);
+                let mut param_data_bytes: Vec<u8> = vec![0u8; param_total_size as usize];
+                reader
+                    .read_exact(&mut param_data_bytes)
+                    .map_err(|e|
+                        AtpError::new(
+                            AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                            "read_bytecode_from_file",
+                            e.to_string()
+                        )
+                    )?;
+
+                params.push(AtpParamTypes::from_bytecode(param_data_bytes)?);
+
+                // Código final para fazer o parsing dos parâmetros
+            }
+
+            let mut token = get_mapping_bytecode_to_token()
+                .get(&instruction_type)
+                .ok_or_else(||
+                    AtpError::new(
+                        AtpErrorCode::TokenNotFound("Invalid Bytecode".into()),
+                        "parse_bytecode_token",
+                        instruction_type.to_string()
+                    )
+                )?();
+
+            token
+                .from_params(&params)
+                .map_err(|e|
+                    AtpError::new(
+                        AtpErrorCode::BytecodeParsingError("Failed Reading Bytecode".into()),
+                        "read_bytecode_from_file",
+                        e.to_string()
+                    )
+                )?;
+
+            result.push(token);
+        }
     }
 
     Ok(result)
