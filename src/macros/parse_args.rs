1f8e1cff2f348a6609607bbc55763e4dfab2dd18
redlintles <treelands54@gmail.com>
2025-12-23 22:53:44 -0300

    Adicionado módulo macros para macros declarativos, adicionado macro parse_args

---
+++ src/macros/parse_args.rs
@@ -0,0 +1,31 @@
+#[macro_export]
+macro_rules! parse_args {
+    ($params:expr, $idx:expr, String, $msg:expr) => {
+        {
+        match &$params[$idx] {
+            AtpParamTypes::String(payload) => payload.clone(),
+            _ => {
+                return Err(AtpError::new(
+                    AtpErrorCode::InvalidParameters($msg.into()),
+                    "",
+                    "",
+                ));
+            }
+        }
+        }
+    };
+    ($params:expr, $idx:expr, Usize, $msg:expr) => {
+        {
+        match &$params[$idx] {
+            AtpParamTypes::Usize(payload) => payload.clone(),
+            _ => {
+                return Err(AtpError::new(
+                    AtpErrorCode::InvalidParameters($msg.into()),
+                    "",
+                    "",
+                ));
+            }
+        }
+        }
+    };
+}
