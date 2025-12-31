f624907787fe2cf24425631b1083704403d8154b
redlintles <treelands54@gmail.com>
2025-12-30 22:05:27 -0300

    Adicionado módulo context

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
 #[cfg(not(feature = "test_access"))]
-mod utils;
-#[cfg(not(feature = "test_access"))]
-mod tokens;
+mod context;
 #[cfg(not(feature = "test_access"))]
 mod globals;
 #[cfg(not(feature = "test_access"))]
-mod context;
+mod macros;
 #[cfg(not(feature = "test_access"))]
 mod text;
 #[cfg(not(feature = "test_access"))]
-mod macros;
+mod tokens;
+#[cfg(not(feature = "test_access"))]
+mod utils;
 
+#[cfg(feature = "test_access")]
+pub mod context;
+#[cfg(feature = "test_access")]
+pub mod globals;
+#[cfg(feature = "test_access")]
+pub mod macros;
 #[cfg(feature = "test_access")]
 pub mod tokens;
 #[cfg(feature = "test_access")]
 pub mod utils;
-#[cfg(feature = "test_access")]
-pub mod globals;
-#[cfg(feature = "test_access")]
-pub mod context;
-#[cfg(feature = "test_access")]
-pub mod macros;
 
 #[cfg(feature = "test_access")]
 pub mod text;
