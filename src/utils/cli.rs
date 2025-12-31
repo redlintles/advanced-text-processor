c546222f03a19e2c71dbd121fe60c81db1f322c8
redlintles <treelands54@gmail.com>
2025-12-31 17:26:14 -0300

    Atualizadas referências à antiga trait AtpBuilderDocs

--- src/utils/cli.rs
+++ src/utils/cli.rs
@@ -1,15 +1,15 @@
 use std::borrow::Cow;
 
 use crate::{
-    builder::atp_processor::{AtpProcessor, AtpProcessorMethods},
-    utils::{errors::AtpError, transforms::get_safe_utf8_char_index},
+    builder::atp_processor::{ AtpProcessor, AtpProcessorMethods },
+    utils::{ errors::AtpError, transforms::get_safe_utf8_char_index },
 };
 
 fn process_run(
     processor: &mut AtpProcessor,
     identifier: &str,
     input: &str,
-    debug: bool,
+    debug: bool
 ) -> Result<String, AtpError> {
     if debug {
         return Ok(processor.process_all_with_debug(identifier, input)?);
@@ -22,7 +22,7 @@ pub fn process_input_single_chunk(
     processor: &mut AtpProcessor,
     identifier: &str,
     input: &str,
-    debug: bool,
+    debug: bool
 ) -> Result<String, AtpError> {
     if input.is_empty() {
         return Ok(String::new());
@@ -34,12 +34,15 @@ pub fn process_input_line_by_line(
     processor: &mut AtpProcessor,
     identifier: &str,
     input: &str,
-    debug: bool,
+    debug: bool
 ) -> Result<String, AtpError> {
     if input.is_empty() {
         return Ok(String::new());
     }
-    let mut text_vec = input.lines().map(|x| x.to_string()).collect::<Vec<_>>();
+    let mut text_vec = input
+        .lines()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>();
 
     for line in text_vec.iter_mut() {
         *line = process_run(processor, identifier, line, debug)?;
@@ -53,17 +56,19 @@ pub fn process_input_by_chunks(
     identifier: &str,
     input: &str,
     chunk_size: usize,
-    debug: bool,
+    debug: bool
 ) -> Result<String, AtpError> {
     if input.is_empty() {
         return Ok(String::new());
     }
     if chunk_size == 0 {
-        return Err(AtpError::new(
-            super::errors::AtpErrorCode::ZeroDivisionError("chunk size == 0".into()),
-            Cow::Borrowed("process_input_by_chunks"),
-            Cow::Owned(input.to_string()),
-        ));
+        return Err(
+            AtpError::new(
+                super::errors::AtpErrorCode::ZeroDivisionError("chunk size == 0".into()),
+                Cow::Borrowed("process_input_by_chunks"),
+                Cow::Owned(input.to_string())
+            )
+        );
     }
     let character_count = input.chars().count();
     let iterations = character_count.div_ceil(chunk_size);
@@ -78,7 +83,7 @@ pub fn process_input_by_chunks(
             processor,
             identifier,
             &input[left_chunk_bound..right_chunk_bound],
-            debug,
+            debug
         )?;
 
         processed.push(result);
@@ -97,8 +102,8 @@ pub fn process_input_by_chunks(
 mod cli_tests {
     mod process_input_by_chunks_tests {
         use crate::{
-            builder::atp_builder::{AtpBuilder, AtpBuilderDocs},
-            utils::{cli::process_input_by_chunks, errors::AtpError},
+            builder::{ atp_builder::{ AtpBuilder }, AtpBuilderMethods },
+            utils::{ cli::process_input_by_chunks, errors::AtpError },
         };
 
         #[test]
@@ -121,8 +126,8 @@ mod cli_tests {
 
     mod process_input_line_by_line_tests {
         use crate::{
-            builder::atp_builder::{AtpBuilder, AtpBuilderDocs},
-            utils::{cli::process_input_line_by_line, errors::AtpError},
+            builder::{ atp_builder::{ AtpBuilder }, AtpBuilderMethods },
+            utils::{ cli::process_input_line_by_line, errors::AtpError },
         };
 
         #[test]
@@ -145,8 +150,8 @@ mod cli_tests {
 
     mod process_input_single_chunk_tests {
         use crate::{
-            builder::atp_builder::{AtpBuilder, AtpBuilderDocs},
-            utils::{cli::process_input_single_chunk, errors::AtpError},
+            builder::{ atp_builder::{ AtpBuilder }, AtpBuilderMethods },
+            utils::{ cli::process_input_single_chunk, errors::AtpError },
         };
 
         #[test]
