4aa9afddb0c593db75bc586d4d59e5424bb39148
redlintles <treelands54@gmail.com>
2026-01-07 19:47:40 -0300

    Implementação inicial de ContextMethods feita

--- src/context/execution_context.rs
+++ src/context/execution_context.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::{tokens::TokenMethods, utils::errors::AtpError};
+use crate::{ tokens::TokenMethods, utils::errors::{ AtpError, AtpErrorCode } };
 
 pub enum VarValues {
     String(String),
@@ -44,14 +44,99 @@ pub trait GlobalContextMethods {
     fn add_to_block(
         &mut self,
         block_id: &str,
-        token: Box<dyn TokenMethods>,
+        token: Box<dyn TokenMethods>
     ) -> Result<(), AtpError>;
     fn get_block(&self, block_id: &str) -> Result<&Vec<Box<dyn TokenMethods>>, AtpError>;
 
-    fn add_var(&mut self, var_entry: VarEntry) -> Result<(), AtpError>;
+    fn add_var(&mut self, id: &str, var_entry: VarEntry) -> Result<(), AtpError>;
     fn get_var(&self, var_id: &str) -> Result<&VarEntry, AtpError>;
     fn get_mut_var(&mut self, var_id: &str) -> Result<&mut VarEntry, AtpError>;
 
     // It would require a more complex implementation. but would help optimizing atp in the future. This will remove data that will no longer be used from the context.
-    fn clean_context(&mut self) -> ();
+    fn clean_context(&mut self) -> () {}
+}
+
+impl GlobalExecutionContext {
+    pub fn new() -> Self {
+        GlobalExecutionContext { variables: HashMap::new(), blocks: HashMap::new() }
+    }
+}
+
+impl GlobalContextMethods for GlobalExecutionContext {
+    fn add_to_block(
+        &mut self,
+        block_id: &str,
+        token: Box<dyn TokenMethods>
+    ) -> Result<(), AtpError> {
+        match self.blocks.get_mut(block_id) {
+            Some(tokens) => {
+                tokens.push(token);
+            }
+            None => {
+                let mut block_vec = Vec::new();
+                block_vec.push(token);
+
+                self.blocks.insert(block_id.to_string(), block_vec);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn get_block(&self, block_id: &str) -> Result<&Vec<Box<dyn TokenMethods>>, AtpError> {
+        match self.blocks.get(block_id) {
+            Some(tokens) => { Ok(tokens) }
+            None => {
+                Err(
+                    AtpError::new(
+                        AtpErrorCode::BlockNotFound("Block not found".into()),
+                        "context.get_block",
+                        block_id.to_string()
+                    )
+                )
+            }
+        }
+    }
+
+    fn add_var(&mut self, id: &str, var_entry: VarEntry) -> Result<(), AtpError> {
+        self.variables.insert(id.to_string(), var_entry);
+        Ok(())
+    }
+
+    fn get_var(&self, var_id: &str) -> Result<&VarEntry, AtpError> {
+        Ok(
+            self.variables
+                .get(var_id)
+                .ok_or_else(||
+                    AtpError::new(
+                        AtpErrorCode::VariableNotFound("Variable not found".into()),
+                        "get_var",
+                        var_id.to_string()
+                    )
+                )?
+        )
+    }
+
+    fn get_mut_var(&mut self, var_id: &str) -> Result<&mut VarEntry, AtpError> {
+        let v = self.variables
+            .get_mut(var_id)
+            .ok_or_else(||
+                AtpError::new(
+                    AtpErrorCode::VariableNotFound("Variable not found".into()),
+                    "get_var",
+                    var_id.to_string()
+                )
+            )?;
+        if v.mutable {
+            Ok(v)
+        } else {
+            Err(
+                AtpError::new(
+                    AtpErrorCode::NonMutableVariableError("Variable is not mutable".into()),
+                    "get_mut_var",
+                    var_id.to_string()
+                )
+            )
+        }
+    }
 }
