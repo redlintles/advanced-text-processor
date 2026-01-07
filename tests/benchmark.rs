aad43f81178fa409294cc9a362e71d4054602339
redlintles <treelands54@gmail.com>
2026-01-07 19:30:07 -0300

    Adicionado novo teste de benchmark debug_overhead_is_too_high

--- tests/benchmark.rs
+++ tests/benchmark.rs
@@ -1,10 +1,79 @@
 #[cfg(feature = "test_access")]
 #[cfg(test)]
 pub mod benchmark {
-    use atp::builder::{ AtpBuilderMethods, atp_processor::{ AtpProcessor, AtpProcessorMethods } };
+    use atp::{
+        builder::{ atp_processor::{ AtpProcessor, AtpProcessorMethods } },
+        utils::test_helpers::build_all_tokens_pipeline_safe,
+    };
     use std::time::Instant;
 
     use atp::utils::errors::AtpError;
+
+    #[test]
+    fn debug_overhead_too_high() -> Result<(), AtpError> {
+        let debug_exectime_cb = || -> Result<f64, AtpError> {
+            let runs = 100;
+
+            let mut total = 0.0;
+
+            let mut processor = AtpProcessor::new();
+
+            let identifier = build_all_tokens_pipeline_safe(&mut processor)?;
+
+            for _ in 0..runs {
+                let start = Instant::now();
+
+                let string_to_process = "Banana Laranja cheia de canja";
+
+                processor.process_all_with_debug(&identifier, string_to_process)?;
+
+                let elapsed = start.elapsed().as_secs_f64();
+
+                total += elapsed;
+            }
+
+            let avg = total / (runs as f64);
+
+            Ok(avg)
+        };
+
+        let no_debug_exectime_cb = || -> Result<f64, AtpError> {
+            let runs = 100;
+
+            let mut total = 0.0;
+
+            let mut processor = AtpProcessor::new();
+
+            let identifier = build_all_tokens_pipeline_safe(&mut processor)?;
+
+            for _ in 0..runs {
+                let start = Instant::now();
+
+                let string_to_process = "Banana Laranja cheia de canja";
+
+                processor.process_all(&identifier, string_to_process)?;
+
+                let elapsed = start.elapsed().as_secs_f64();
+
+                total += elapsed;
+            }
+
+            let avg = total / (runs as f64);
+            Ok(avg)
+        };
+
+        let debug_exectime = debug_exectime_cb()?;
+        let no_debug_exectime = no_debug_exectime_cb()?;
+
+        let max_tolerance = no_debug_exectime * 100.0;
+
+        println!("Execução do método process_all_with_debug: {:.6}", debug_exectime);
+        println!("Execução do método process_all: {:.6}", no_debug_exectime);
+        println!("Tolerância máxima: {:.6}", max_tolerance);
+
+        assert!(debug_exectime < max_tolerance, "Overhead do debug muito alto");
+        Ok(())
+    }
 
     #[test]
     fn process_sbs_all_tokens() -> Result<(), AtpError> {
@@ -14,58 +83,7 @@ pub mod benchmark {
 
         let mut processor = AtpProcessor::new();
 
-        let identifier = processor
-            .create_pipeline()
-            .add_to_beginning("Banana")?
-            .add_to_end("pizza")?
-            .repeat(3)?
-            .delete_after(20 as usize)?
-            .delete_before(3 as usize)?
-            .delete_chunk(0 as usize, 3 as usize)?
-            .delete_first()?
-            .delete_last()?
-            .replace_all_with(r"a", "e")?
-            .replace_first_with("L", "coxinha")?
-            .replace_count_with("e", "carro", 3)?
-            .insert(0, "Coxinha Banana")?
-            .rotate_left(1 as usize)?
-            .rotate_right(2 as usize)?
-            .trim_both_sides()?
-            .trim_left_side()?
-            .trim_right_side()?
-            .add_to_beginning("laranjadebananavermelha")?
-            .select(3, 7)?
-            .replace_count_with("a", "b", 3)?
-            .to_uppercase_all()?
-            .to_lowercase_all()?
-            .to_uppercase_single(3)?
-            .to_lowercase_single(2)?
-            .capitalize_first_word()?
-            .capitalize_single_word(1)?
-            .capitalize_last_word()?
-            .capitalize_range(1, 3)?
-            .split_select("B", 1)?
-            .capitalize_chunk(1, 3)?
-            .replace_last_with("b", "c")?
-            .replace_nth_with("b", "d", 3)?
-            .to_url_encoded()?
-            .to_url_decoded()?
-            .to_reverse()?
-            .split_characters()?
-            .to_html_escaped()?
-            .to_html_unescaped()?
-            .to_json_escaped()?
-            .to_json_unescaped()?
-            .insert(1, "banana")?
-            .to_uppercase_chunk(1, 3)?
-            .to_lowercase_chunk(0, 5)?
-            .join_to_camel_case()?
-            .join_to_kebab_case()?
-            .join_to_pascal_case()?
-            .join_to_snake_case()?
-            .pad_left("xy", 12)?
-            .pad_right("yx", 20)?
-            .build();
+        let identifier = build_all_tokens_pipeline_safe(&mut processor)?;
 
         for _ in 0..runs {
             let start = Instant::now();
@@ -87,7 +105,7 @@ pub mod benchmark {
 
         println!("Média: {:.6} Segundos", avg);
 
-        assert!(avg < 0.003, "Executou muito devagar");
+        assert!(avg < 0.03, "Executou muito devagar");
         Ok(())
     }
     #[test]
@@ -98,65 +116,14 @@ pub mod benchmark {
 
         let mut processor = AtpProcessor::new();
 
-        let identifier = processor
-            .create_pipeline()
-            .add_to_beginning("Banana")?
-            .add_to_end("pizza")?
-            .repeat(3)?
-            .delete_after(20 as usize)?
-            .delete_before(3 as usize)?
-            .delete_chunk(0 as usize, 3 as usize)?
-            .delete_first()?
-            .delete_last()?
-            .replace_all_with(r"a", "e")?
-            .replace_first_with("L", "coxinha")?
-            .replace_count_with("e", "carro", 3)?
-            .insert(0, "Coxinha Banana")?
-            .rotate_left(1 as usize)?
-            .rotate_right(2 as usize)?
-            .trim_both_sides()?
-            .trim_left_side()?
-            .trim_right_side()?
-            .add_to_beginning("laranjadebananavermelha")?
-            .select(3, 7)?
-            .replace_count_with("a", "b", 3)?
-            .to_uppercase_all()?
-            .to_lowercase_all()?
-            .to_uppercase_single(3)?
-            .to_lowercase_single(2)?
-            .capitalize_first_word()?
-            .capitalize_single_word(1)?
-            .capitalize_last_word()?
-            .capitalize_range(1, 3)?
-            .split_select("B", 1)?
-            .capitalize_chunk(1, 3)?
-            .replace_last_with("b", "c")?
-            .replace_nth_with("b", "d", 3)?
-            .to_url_encoded()?
-            .to_url_decoded()?
-            .to_reverse()?
-            .split_characters()?
-            .to_html_escaped()?
-            .to_html_unescaped()?
-            .to_json_escaped()?
-            .to_json_unescaped()?
-            .insert(1, "banana")?
-            .to_uppercase_chunk(1, 3)?
-            .to_lowercase_chunk(0, 5)?
-            .join_to_camel_case()?
-            .join_to_kebab_case()?
-            .join_to_pascal_case()?
-            .join_to_snake_case()?
-            .pad_left("xy", 12)?
-            .pad_right("yx", 20)?
-            .build();
+        let identifier = build_all_tokens_pipeline_safe(&mut processor)?;
 
         for _ in 0..runs {
             let start = Instant::now();
 
             let string_to_process = "Banana Laranja cheia de canja";
 
-            processor.process_all(&identifier, string_to_process).unwrap();
+            processor.process_all(&identifier, string_to_process)?;
 
             let elapsed = start.elapsed().as_secs_f64();
 
